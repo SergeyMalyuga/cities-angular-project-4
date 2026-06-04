6204230d008329b6e0f19ac80dcd16ce6e4f5d63
Sergey Malyuga <sergey@malyuga.ru>
2026-06-04 15:23:33 +0300

    feat(app): создать глобальный store

---
+++ src/app/store/app/selectors/app.selectors.ts
@@ -0,0 +1,17 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {OfferPreview} from '../../../core/models/offers';
+
+const selectOffersState = createFeatureSelector<AppState['offers']>('offers');
+const selectCityState = createFeatureSelector<AppState['currentCity']>('currentCity');
+
+export const selectOffersByCity = createSelector(
+  selectOffersState,
+  selectCityState,
+  (offerState, cityState) =>
+    Object.values(offerState.entities).filter((offer): offer is OfferPreview => {
+      if (offer === undefined) return false;
+      return offer.city.name === cityState.name;
+    })
+)
+
