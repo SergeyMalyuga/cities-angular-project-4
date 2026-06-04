de34cfa08bfc2fbf1985b78103627a58f4e32dd6
Sergey Malyuga <sergey@malyuga.ru>
2026-06-04 15:14:28 +0300

    feat(offer-card): создать компонент карточки оффера

---
+++ src/app/shared/offer-card/offer-card.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {OfferPreview} from '../../core/models/offers';
+import {TitleCasePipe} from '@angular/common';
+
+@Component({
+  selector: 'app-offer-card',
+  templateUrl: './offer-card.component.html',
+  imports: [
+    TitleCasePipe
+  ]
+})
+export class OfferCardComponent {
+  @Input({required: true}) offer!: OfferPreview;
+  protected readonly Math = Math;
+}
