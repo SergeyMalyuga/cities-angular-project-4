3f17647939682888f2d71bfe881e890db5a13fe8
Sergey Malyuga <sergey@malyuga.ru>
2026-06-05 15:54:37 +0300

    feat(offer-card): добавить отслеживание наведения на карточку

--- src/app/shared/offer-card/offer-card.component.ts
+++ src/app/shared/offer-card/offer-card.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
-import { OfferPreview } from '../../core/models/offers';
-import { TitleCasePipe } from '@angular/common';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {OfferPreview} from '../../core/models/offers';
+import {TitleCasePipe} from '@angular/common';
+import {HoverTrackerDirective} from '../directives/hover-tracker.directive';
 
 @Component({
   selector: 'app-offer-card',
   templateUrl: './offer-card.component.html',
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, HoverTrackerDirective],
 })
 export class OfferCardComponent {
-  @Input({ required: true }) offer!: OfferPreview;
+  @Input({required: true}) offer!: OfferPreview;
+  @Output() hovered = new EventEmitter<OfferPreview | null>();
+
   protected readonly Math = Math;
+
+  public onHovered(isHovered: boolean) {
+    if (isHovered) {
+      this.hovered.emit(this.offer);
+    } else {
+      this.hovered.emit(null);
+    }
+  }
 }
