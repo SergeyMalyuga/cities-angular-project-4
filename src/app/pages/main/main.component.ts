ec22c9519c55c8710cc2fccef456edebd9d60e9a
Sergey Malyuga <sergey@malyuga.ru>
2026-06-05 10:52:58 +0300

    feat(home-page): реализовать отображение офферов при смене города

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,18 +1,28 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { HeaderComponent } from '../../shared/header/header.component';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../core/models/app.state';
-import { selectOffersByCity } from '../../store/app/selectors/app.selectors';
-import { OfferCardComponent } from '../../shared/offer-card/offer-card.component';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {HeaderComponent} from '../../shared/header/header.component';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectCurrentCity, selectOffersByCity} from '../../store/app/selectors/app.selectors';
+import {OfferCardComponent} from '../../shared/offer-card/offer-card.component';
+import {CITY_LOCATIONS} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
+import {City} from '../../core/models/city';
+import {changeCity} from '../../store/city/actions/city.actions';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, OfferCardComponent],
+  imports: [HeaderComponent, OfferCardComponent, NgClass],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
   private store = inject(Store<AppState>);
+  protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
 
   public offers = this.store.selectSignal(selectOffersByCity);
+  public currentCity = this.store.selectSignal(selectCurrentCity);
+
+  public changeCity(city: City) {
+    this.store.dispatch(changeCity({city}));
+  }
 }
