40933c20d6a99bc489cadc3bb76058a748c5a2aa
Sergey Malyuga <sergey@malyuga.ru>
2026-06-09 14:10:25 +0300

    feat(user): расширить типы свойства error в модели пользователя

--- src/app/core/models/user.state.ts
+++ src/app/core/models/user.state.ts
@@ -1,9 +1,10 @@
 import { User } from './user';
 import { AuthorizationStatus } from '../constants/const';
+import {HttpErrorResponse} from '@angular/common/http';
 
 export interface UserState {
   authorizationStatus: AuthorizationStatus;
   user: User | undefined;
-  error: string | null;
+  error: HttpErrorResponse | string | null;
   isLoading: boolean;
 }
