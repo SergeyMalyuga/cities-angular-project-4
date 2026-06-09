a5560c7bddc9f8b1f4389c26a4a8bbb1bc012d67
Sergey Malyuga <sergey@malyuga.ru>
2026-06-09 12:17:04 +0300

    feat(header): настроить навигацию
    - Переход на главную страницу
    - Переход на страницу аутентификации

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
+import {RouterLink} from '@angular/router';
+import {AppRoute, AuthorizationStatus} from '../../core/constants/const';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private store = inject(Store);
+
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+  protected readonly AppRoute = AppRoute;
+  protected readonly AuthorizationStatus = AuthorizationStatus;
+}
