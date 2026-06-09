d8ddbf8c9fafb71dcfbdda0bdd399f01ab895f51
Sergey Malyuga <sergey@malyuga.ru>
2026-06-09 14:13:15 +0300

    feat(login): реализовать логику аутентификацию пользователя

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {login} from '../../store/user/actions/user.actions';
+import {Credentials} from '../../core/models/credentials';
+import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
+import {filter, first, take} from 'rxjs';
+import {AppRoute, AuthorizationStatus} from '../../core/constants/const';
+import {loadOffers} from '../../store/offer/actions/offer.actions';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private store = inject(Store<AppState>);
+  private router = inject(Router);
+
+  public loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$')]],
+  });
+
+  public ngOnInit(): void {
+    this.store.select(selectAuthStatus).pipe(first(status => status === AuthorizationStatus.AUTH))
+      .subscribe(() => {
+      this.loginForm.reset();
+      this.store.dispatch(loadOffers());
+      this.router.navigate([AppRoute.MAIN]);
+    })
+  }
+
+  public onSubmit(): void {
+    if (this.loginForm.valid) {
+      const credentials: Credentials = this.loginForm.value;
+      this.store.dispatch(login({credentials}))
+    }
+  }
+
+  public get passwordControl() {
+    return this.loginForm.get('password')
+  }
+
+  public get emailControl() {
+    return this.loginForm.get('email')
+  }
+}
