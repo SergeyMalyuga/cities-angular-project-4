a992b95fe1f9fb01283a0e2a0cac902b7b86cfc5
Sergey Malyuga <sergey@malyuga.ru>
2026-06-10 10:50:56 +0300

    chore(favorite-offer): переименовать сущности стейта в единственное число

---
+++ src/app/core/models/favorite-offer.state.ts
@@ -0,0 +1,8 @@
+import { EntityState } from '@ngrx/entity';
+import { OfferPreview } from './offers';
+import {HttpErrorResponse} from '@angular/common/http';
+
+export interface FavoriteOfferState extends EntityState<OfferPreview> {
+  isLoading: boolean;
+  error: HttpErrorResponse | string | null;
+}
