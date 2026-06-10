e5f748b1efc4f3da753a36768f0dc5d7bdc2acdb
Sergey Malyuga <sergey@malyuga.ru>
2026-06-10 14:41:23 +0300

    feat(offer-card): реализовать переход на страницу выбранного оффера

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -2,11 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { OfferPreview } from '../../../core/models/offers';
 import { TitleCasePipe } from '@angular/common';
 import { HoverTrackerDirective } from '../../directives/hover-tracker.directive';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
 
 @Component({
   selector: 'app-offer-card',
   templateUrl: './offer-card.component.html',
-  imports: [TitleCasePipe, HoverTrackerDirective],
+  imports: [TitleCasePipe, HoverTrackerDirective, RouterLink],
 })
 export class OfferCardComponent {
   @Input({ required: true }) offer!: OfferPreview;
@@ -21,4 +23,6 @@ export class OfferCardComponent {
       this.hovered.emit(null);
     }
   }
+
+  protected readonly AppRoute = AppRoute;
 }
