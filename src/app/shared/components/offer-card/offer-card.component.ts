c6090e1c7a3ca1b2182f4eaf8402d9df96e7a4cf
Sergey Malyuga <sergey@malyuga.ru>
2026-06-15 11:43:26 +0300

    feat(offer-card): реализовать изменение и отображение статуса избранного оффера

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,20 +1,32 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { OfferPreview } from '../../../core/models/offers';
-import { TitleCasePipe } from '@angular/common';
-import { HoverTrackerDirective } from '../../directives/hover-tracker.directive';
-import { RouterLink } from '@angular/router';
-import { AppRoute } from '../../../core/constants/const';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {OfferPreview} from '../../../core/models/offers';
+import {NgClass, TitleCasePipe} from '@angular/common';
+import {HoverTrackerDirective} from '../../directives/hover-tracker.directive';
+import {RouterLink} from '@angular/router';
+import {AppRoute, AuthorizationStatus} from '../../../core/constants/const';
+import {OfferService} from '../../../core/services/offer.service';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {selectIsFavoriteOffersLoading} from '../../../store/favorite-offer/selectors/favorite-offer.selectors';
+import {selectAuthStatus} from '../../../store/user/selectors/user.selectors';
 
 @Component({
   selector: 'app-offer-card',
   templateUrl: './offer-card.component.html',
-  imports: [TitleCasePipe, HoverTrackerDirective, RouterLink],
+  imports: [TitleCasePipe, HoverTrackerDirective, RouterLink, NgClass],
 })
 export class OfferCardComponent {
-  @Input({ required: true }) offer!: OfferPreview;
+  @Input({required: true}) offer!: OfferPreview;
   @Output() hovered = new EventEmitter<OfferPreview | null>();
 
+  private offerService = inject(OfferService);
+  private store = inject(Store<AppState>);
+
   protected readonly Math = Math;
+  protected readonly AppRoute = AppRoute;
+
+  public isFavoriteOffersLoading = this.store.selectSignal(selectIsFavoriteOffersLoading);
+  public authStatus = this.store.selectSignal(selectAuthStatus);
 
   public onHovered(isHovered: boolean) {
     if (isHovered) {
@@ -24,5 +36,9 @@ export class OfferCardComponent {
     }
   }
 
-  protected readonly AppRoute = AppRoute;
+  public toggleFavoriteOffer() {
+    this.offerService.toggleFavorite(this.offer.id, !this.offer.isFavorite);
+  }
+
+  protected readonly AuthorizationStatus = AuthorizationStatus;
 }
